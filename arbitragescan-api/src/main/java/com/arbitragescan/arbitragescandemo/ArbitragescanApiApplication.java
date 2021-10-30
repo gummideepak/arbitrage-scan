634a784c7703fc5dad44232afeca05948fefcae5
deepak <dg67@njit.edu>
2021-10-29 20:31:01 -0400

    Initial project setup
    Created Both Angular and Spring Boot Projects

---
+++ arbitragescan-api/src/main/java/com/arbitragescan/arbitragescandemo/ArbitragescanApiApplication.java
@@ -0,0 +1,13 @@
+package com.arbitragescan.arbitragescandemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbitragescanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbitragescanApiApplication.class, args);
+	}
+
+}
---
+++ arbitragescan-api/src/test/java/com/arbitragescan/arbitragescandemo/ArbitragescanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbitragescan.arbitragescandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbitragescanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
